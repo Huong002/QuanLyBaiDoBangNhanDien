1fb9da5c45b44beb94ee54c1d23bb7ea63393787
nguyenhoangtamm <nguyenhoangtam12cb3@gmail.com>
2025-08-14 21:29:07 +0700

    Remove unused files and assets from the project, including training results, images, styles, and HTML templates. This cleanup helps streamline the codebase and eliminates unnecessary dependencies.

--- config/database.py
+++
@@ -1,8 +0,0 @@
-# config/database.py
-DB_CONFIG = {
-    "host": "localhost",
-    "database": "machine",
-    "user": "postgres",  
-    "password": "150600",   
-    "port": "5432"
-}
--- readme.md
+++
@@ -1,5 +0,0 @@
-set FLASK_APP=create_db.py
-# Khởi tạo thư mục migration nếu chưa có
-flask db init
-# chạy migration
-flask db migrate -m "Initial migration"
